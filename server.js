e278fd70c806682672c46e5aaec5574cabf5bb83
Hemant <sonujoshi9785@gmail.com>
2023-08-12 11:36:18 +0530

    changed origin to new

--- server.js
+++ server.js
@@ -49,7 +49,8 @@ if (process.env.NODE_ENV === "production") {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "https://i-chat-frontend-orli.vercel.app",
     credentials: true, //access-control-allow-credentials:true
     optionSuccessStatus: 200,
     // origin: "http://localhost:3000",
